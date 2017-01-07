7be6129838f06bc0ba0bdd3cf39de4f0e5fc5e91
sriram401 <sriramt@live.com>
2017-01-06 19:22:25 -0500

    Added Collections

--- src/test.kt
+++ src/test.kt
@@ -9,6 +9,19 @@ fun main(args : Array<String>)
     loopTest(5)
     val p1 = Person("Sriram")
     println("Person One name : " + p1.name)
+
+    val l1 = listOf<String>("testListItem1","testListItem2")
+    println("List Items : " +l1)
+    println("java class for list : " + l1.javaClass)
+
+    val s1 = setOf<String>("testSetItem1","testSetItem2")
+    println("Sett Items : " +s1)
+    println("java class for set : " + s1.javaClass)
+
+    val m1 = mapOf<String,String>("k1" to "v1", "k2" to "v2")
+    println("Map Items : " + m1)
+    println("java class for map : " + m1.javaClass)
+
 }
 
 fun loopTest(a : Int)
